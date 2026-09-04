819c1bce77e88dea604b5ee3aa48d1da7606a9e8
Arnur Omirserikov <arnuromirserikov@Arnurs-MacBook-Air.local>
2026-09-04 11:53:00 +0500

    Implemented Logging In and Out with Puter

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import {
   isRouteErrorResponse,
   Links,
@@ -8,6 +10,8 @@ import {
 } from "react-router";
 
 import type { Route } from "./+types/root";
+import { usePuterStore } from "./lib/puter";
+
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
@@ -24,6 +28,11 @@ export const links: Route.LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const { init } = usePuterStore();
+
+  React.useEffect(() => {
+    init();
+  }, [init]);
   return (
     <html lang="en">
       <head>
@@ -33,6 +42,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <script src="https://js.puter.com/v2/"></script>
         {children}
         <ScrollRestoration />
         <Scripts />
